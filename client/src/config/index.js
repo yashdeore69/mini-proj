81d16b7055fbc782fabb1606ced51168cdbdad0e
yashdeore69 <deoreyash2003@gmail.com>
2025-04-29 18:30:12 +0530

    third commit: login form incomplete code

---
+++ client/src/config/index.js
@@ -0,0 +1,25 @@
+export const registerFormControls = [
+    {
+        name: 'username',
+        type: 'text',
+        label: 'Username',
+        placeholder: 'Enter your username',
+        componentType: 'input',
+    },
+
+    {
+        name: "email",
+        label: "Email",
+        placeholder: "Enter your email",
+        componentType: "input",
+        type: "email",
+      },
+
+      {
+        name: "password",
+        label: "Password",
+        placeholder: "Enter your password",
+        componentType: "input",
+        type: "password",
+      },
+]
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,8 +1,45 @@
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+    name: "",
+    email: "",
+    password: "",
+};
 
 function AuthRegister() {
+    const [formData, setFormData] = useState(initialState)
+
+    function onSubmit() {
+
+    }
+
+
     return (
-        <div>
-            <h1>Register</h1>
+    <div className="mx-auto w-full max-w-md space-y-6">
+        <div className="text-center">
+            <h1 className="text-3xl font-bold tracking-tight text-foreground">
+            Create new account
+            </h1>
+            <p className="mt-2">
+            Already have an account
+            <Link 
+                className="font-medium ml-2 text-primary hover:underline" 
+                to='/auth/login'
+            >
+                Login
+            </Link>
+            </p>
+        </div>
+        <CommonForm
+            formControls={registerFormControls}
+            buttonText={"Sign Up"}
+            formData={formData}
+            setFormData={setFormData}
+            onSubmit={onSubmit}
+        />
         </div>
     )
 }
